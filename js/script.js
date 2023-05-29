6fda0a8120be51b930ed495420b7b80ebddec421
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-29 15:21:38 +0200

    printing every member's feat in console

--- js/script.js
+++ js/script.js
@@ -3,31 +3,31 @@ let team = [
         name: "Wayne Barnett",
         role: "Founder & CEO",
         photo: "wayne-barnett-founder-ceo.jpg"
-    }
+    },
 
     {
         name: "Angela Caroll",
         role: "Chief Editor",
         photo: "angela-caroll-chief-editor.jpg"
-    }
+    },
 
     {
         name: "Walter Gordon",
         role: "Office Manager",
         photo: "walter-gordon-office-manager.jpg"
-    }
+    },
 
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
         photo: "angela-lopez-social-media-manager.jpg"
-    }
+    },
 
     {
         name: "Scott Estrada",
         role: "Developer",
         photo: "scott-estrada-developer.jpg"
-    }
+    },
 
     {
         name: "Barbara Ramos",
@@ -35,3 +35,8 @@ let team = [
         photo: "barbara-ramos-graphic-designer.jpg"
     }
 ]
+
+for (let i = 1; i < team.length; i++){
+    console.log(team[i].name + ", " + team[i].role + ", " + team[i].photo);
+}
+
